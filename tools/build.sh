56bdee9245ef059a4cd2180ea547305af6af53dd
Ben Prisby <ben@benprisby.com>
2022-10-31 16:56:16 -0400

    Build script refactoring.

--- tools/build.sh
+++ tools/build.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
-# Move to the current working directory.
-cd $(dirname ${BASH_SOURCE[ ${#BASH_SOURCE[@]} - 1 ]})
+set -e
+
+# Ensure Qt is available in the PATH.
+if ! command -v qmake &> /dev/null; then
+    echo "ERROR: Failed to find qmake. Ensure Qt is accessible in the PATH and try again."
+    exit 1
+fi
+
+# Move to the repository root.
+cd "$(dirname "$0")/.."
 
 # Clean up any previous build artifacts.
-if [ -d ../build ]; then
-    rm -rf ../build
-fi
-if [ -d ../bin ]; then
-    rm -rf ../bin
-fi
+rm -rf bin build
 
-# Build the application
-mkdir ../build
-cd ../build
+# Build the application.
+mkdir build
+cd build
 qmake ..
-make -j4 install
-
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+    cpu_count=$(sysctl -n hw.physicalcpu)
+else
+    cpu_count=$(nproc)
+fi
+make -j${cpu_count} install
