bf3fb8a6a98986198d0b61c6b0fecc8904baaf15
Ben Prisby <ben@benprisby.com>
2022-10-31 17:03:35 -0400

    Package script refactoring.

--- tools/package.sh
+++ tools/package.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Move to the current working directory.
-cd $(dirname ${BASH_SOURCE[ ${#BASH_SOURCE[@]} - 1 ]})
+archive_filename=vicecitydashboard-src.tar.gz
+
+# Move to the repository root.
+cd "$(dirname "$0")/.."
+checkout_dir="$(basename $(pwd))"
 
 # Archive the repository, excluding build artifacts.
-cd ../..
-tar cvzf vcdashboard-src.tar.gz --exclude=ViceCityDashboard/build --exclude=ViceCityDashboard/bin ViceCityDashboard
-
+cd ..
+tar cvzf ${archive_filename} --exclude="${checkout_dir}/build" --exclude="${checkout_dir}/bin" "${checkout_dir}"
+mv -v ${archive_filename} "${checkout_dir}"
